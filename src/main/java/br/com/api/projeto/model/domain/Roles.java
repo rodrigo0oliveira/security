ef482b86ee3c13df92f5e8ae79c1c8ff15a2bc6a
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-09-08 12:20:28 -0300

    Update pom.xml,add dependencies and create domain entities

---
+++ src/main/java/br/com/api/projeto/model/domain/Roles.java
@@ -0,0 +1,53 @@
+package br.com.api.projeto.model.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_roles")
+public class Roles implements GrantedAuthority{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter 
+	@Column(name = "role_id")
+	private String id;
+	
+	@Column(name = "name")
+	@Getter @Setter 
+	private String name;
+	
+
+	public Roles(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String getAuthority() {
+		return name;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/br/com/api/projeto/model/domain/User.java
@@ -0,0 +1,93 @@
+package br.com.api.projeto.model.domain;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_users")
+public class User implements UserDetails{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	@Column(name = "user_id")
+	private String id;
+	
+	@Column(name = "username")
+	@Getter @Setter
+	private String username;
+	
+	@Column(name = "password")
+	@Getter @Setter
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_users_roles",
+	joinColumns = @JoinColumn(name="user_id"),
+	inverseJoinColumns = @JoinColumn(name="role_id"))
+	private Collection<Roles> roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+	
+	@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+	
+	
+
+}
