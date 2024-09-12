f181708ea3715b428ee7139e96b0ac95203ae20b
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-09-12 12:04:00 -0300

    Create query for search user for your username

--- src/main/java/br/com/api/projeto/model/repository/UserRepository.java
+++ src/main/java/br/com/api/projeto/model/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package br.com.api.projeto.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.api.projeto.model.domain.User;
 
 public interface UserRepository extends JpaRepository<User, String>{
+	
+	Optional<User> findByusername(String userName);
 
 }
