467d0dd5ebdcfdffb1ee784bfbe443354ad928af
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-09-12 12:09:01 -0300

    Create UserService implements UserDetailsService

---
+++ src/main/java/br/com/api/projeto/model/services/UserService.java
@@ -0,0 +1,24 @@
+package br.com.api.projeto.model.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.api.projeto.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService{
+	
+	private final UserRepository userRepository;
+	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByusername(username)
+				.orElseThrow(()-> new UsernameNotFoundException("User :"+username +" not found"));
+	}
+
+}
