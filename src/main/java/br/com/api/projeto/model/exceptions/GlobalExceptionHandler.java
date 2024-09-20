f379009cfc547d3a91823157a6a426aeaca43520
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-09-20 11:43:08 -0300

    Create GlobalException Handler

---
+++ src/main/java/br/com/api/projeto/model/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.api.projeto.model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, Object>> argumentNotValidException(MethodArgumentNotValidException exception){
+		Map<String, Object> errors = new HashMap<String, Object>();
+		errors.put("status", HttpStatus.BAD_REQUEST);
+		
+		Map<String,Object> detailErrors = new HashMap<String, Object>();
+		
+		exception.getBindingResult().getFieldErrors().stream().forEach(error->{
+			detailErrors.put(error.getField(), error.getDefaultMessage());
+		});
+		
+		errors.put("Error Details",detailErrors);
+		
+		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+
+}
+}
