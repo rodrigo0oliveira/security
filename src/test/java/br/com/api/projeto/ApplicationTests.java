a48fe7414abebde75a39723a94123c71e38b953a
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-09-24 14:09:10 -0300

    Update

--- src/test/java/br/com/api/projeto/ApplicationTests.java
+++ src/test/java/br/com/api/projeto/ApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.api.projeto;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
