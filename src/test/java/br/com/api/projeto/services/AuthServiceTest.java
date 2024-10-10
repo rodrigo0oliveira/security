46174a28908658fbe9d8ccc06f5eb5f8968747fb
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-10-10 19:08:31 -0300

    Updates in AuthServiceTest

--- src/test/java/br/com/api/projeto/services/AuthServiceTest.java
+++ src/test/java/br/com/api/projeto/services/AuthServiceTest.java
@@ -11,8 +11,8 @@ import br.com.api.projeto.model.security.TokenResponse;
 import br.com.api.projeto.model.services.AuthService;
 import br.com.api.projeto.model.services.EmailService;
 import br.com.api.projeto.model.services.RoleService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,15 +23,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
 import java.util.Collections;
 import java.util.Optional;
 import java.util.UUID;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -107,7 +105,43 @@ public class AuthServiceTest {
 
         Assert.assertEquals(actualMessage,expectedMessage);
 
+    }
 
+    @Test
+    void testLoginWhenCredentialsIsValid() throws Exception {
+        LoginDto loginDto = new LoginDto(user.getUsername(),user.getPassword());
+        TokenResponse tokenResponse = new TokenResponse("mock-token",6000,loginDto.getUsername());
+
+        when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto
+                .getUsername(),loginDto.getPassword()))).thenReturn(authentication);
+
+        when(tokenProvider.generateToken(authentication)).thenReturn(tokenResponse);
+
+        TokenResponse actualToken = authService.login(loginDto);
+
+        Assert.assertEquals(tokenResponse,actualToken);
+        verify(authenticationManager).authenticate(any(Authentication.class));
+        verify(tokenProvider).generateToken(any(Authentication.class));
+
+    }
+
+    @Test
+    void testLoginWhenCredentialsIsNotValid() throws JsonProcessingException {
+        LoginDto loginDto = new LoginDto(null,"133");
+        TokenResponse tokenResponse = new TokenResponse("mock-token",6000,loginDto.getUsername());
+
+        lenient().when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(),loginDto
+                .getPassword()))).thenReturn(authentication);
+
+        when(tokenProvider.generateToken(authentication)).thenThrow(new AuthenticationException("Credenciais inválidas") {
+        });
+
+        AuthenticationException authenticationException = Assert.assertThrows(AuthenticationException.class,
+                ()->tokenProvider.generateToken(authentication));
+
+        String expectedMessage = "Credenciais inválidas";
+
+        Assert.assertEquals(expectedMessage,authenticationException.getMessage());
     }
 
     @Test
@@ -134,5 +168,13 @@ public class AuthServiceTest {
         Assert.assertEquals(expectedMessage,actualMessage);
 
     }
+
+    @Test
+    void testGenerateRandomPassword(){
+        String ramdomPassword = authService.generateRandomPassoword();
+        Assert.assertEquals(ramdomPassword.length(),10);
+    }
+
+
 
 }
