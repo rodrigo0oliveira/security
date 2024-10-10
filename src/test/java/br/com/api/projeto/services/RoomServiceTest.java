0b03474039796d2fc939ee9d51ffd8372ee215d0
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-10-10 11:31:45 -0300

    Create tests for ReserveService and RoomService

---
+++ src/test/java/br/com/api/projeto/services/RoomServiceTest.java
@@ -0,0 +1,106 @@
+package br.com.api.projeto.services;
+
+import br.com.api.projeto.model.domain.Room;
+import br.com.api.projeto.model.domain.dto.RoomDto;
+import br.com.api.projeto.model.domain.enums.RoomType;
+import br.com.api.projeto.model.domain.enums.Status;
+import br.com.api.projeto.model.repository.RoomRepository;
+import br.com.api.projeto.model.services.RoomService;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RoomServiceTest {
+
+    Room room;
+
+    @InjectMocks
+    @Spy
+    RoomService roomService;
+
+    @Mock
+    RoomRepository roomRepository;
+
+    @BeforeEach
+    void setup(){
+        room = Room.builder().id(UUID.randomUUID().toString())
+                .roomnumber("1")
+                .roomtype(RoomType.SOLTEIRO)
+                .dailyPrice(new BigDecimal("300"))
+                .status(Status.DISPONÍVEL)
+                .build();
+    }
+
+    @Test
+    void testCreateRoomShouldBeSucess(){
+
+        when(roomRepository.save(any(Room.class))).thenReturn(room);
+
+
+        String message = roomService.createRoom(room);
+        String expected = "Quarto criado";
+
+        Assertions.assertEquals(message, expected);
+
+        verify(roomRepository).save(any(Room.class));
+
+    }
+
+    @Test
+    void testEditRoomShouldReturnRoomNotFound(){
+
+       when(roomRepository.findByroomnumber(any(String.class))).thenReturn(null);
+
+       String actualMessage = roomService.editRoom("1",room);
+       String expectedMessage = "Quarto não encontrado";
+
+        Assert.assertEquals(actualMessage,expectedMessage);
+
+    }
+
+    @Test
+    void testEditRoomShouldReturnRoomEdited(){
+
+        when(roomRepository.findByroomnumber(any(String.class))).thenReturn(room);
+        doNothing().when(roomService).editRoomInformation(room,room);
+        when(roomRepository.save(any(Room.class))).thenReturn(room);
+
+        String actualMessage = roomService.editRoom("1",room);
+        String expectedMessage = "Quarto atualizado";
+
+        Assert.assertEquals(actualMessage,expectedMessage);
+
+    }
+
+    @Test
+    void testFindRoomsToAuthenticateShouldReturnDtoList(){
+
+        when(roomRepository.findAll()).thenReturn(Collections.singletonList(room));
+
+        RoomDto expected = new RoomDto(room.getRoomnumber(),room.getDailyPrice(),room.getRoomtype());
+
+        List<RoomDto> list = roomService.findAllRoomToUserAuthenticate();
+        RoomDto actual = list.get(0);
+
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+
+}
