816b1d0ef3f210d8a7e181e1da82b653f9499560
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-10-14 09:43:30 -0300

    Create test for RoomRepository

---
+++ src/test/java/br/com/api/projeto/repository/RoomRepositoryTest.java
@@ -0,0 +1,57 @@
+package br.com.api.projeto.repository;
+
+import br.com.api.projeto.model.domain.Room;
+import br.com.api.projeto.model.domain.enums.RoomType;
+import br.com.api.projeto.model.domain.enums.Status;
+import br.com.api.projeto.model.repository.RoomRepository;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+
+@DataJpaTest
+public class RoomRepositoryTest {
+
+    RoomRepository roomRepository;
+
+    Room room;
+
+    @Autowired
+    RoomRepositoryTest(RoomRepository roomRepository){
+        this.roomRepository = roomRepository;
+    }
+
+    @BeforeEach
+    void setup(){
+        room = Room.builder()
+                .roomtype(RoomType.SOLTEIRO)
+                .roomnumber("4")
+                .dailyPrice(new BigDecimal("30"))
+                .status(Status.DISPONÍVEL)
+                .build();
+
+        roomRepository.save(room);
+    }
+
+    @Test
+    void testFindByRoomnumberShouldReturnRoom(){
+        Room actualRoom = roomRepository.findByroomnumber("4");
+
+        assertEquals(room.toString(),actualRoom.toString());
+        assertNotEquals(actualRoom.getId(),0);
+
+    }
+
+    @Test
+    void testFindByRoomNumberShouldReturnNull(){
+        Room actualRoom = roomRepository.findByroomnumber("1");
+
+        assertEquals(null,actualRoom);
+
+    }
+
+}
